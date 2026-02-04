58095c85907c8bd8739841b8c103a06cd09a2f05
Narendra Sharma <154484787+NarendraSharma52@users.noreply.github.com>
2026-02-04 17:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 //your JS code here. If required.
-const otp = document.querySelectorAll(".code");
-// console.log(otp);
-// console.log(otp[0]);
-// console.dir(otp[0]);
-otp[0].focus();
+const inputs = document.querySelectorAll(".code");
 
-otp.forEach((num, index) => {
-    // console.log(num, index);
-    num.addEventListener("input", (event) => {
-        // console.dir(event.target);
-        console.log(event.target.value);
-        if(event.target.value.length === 1 && index < otp.length - 1) {
-            otp[index+1].focus();
-        }
-    })
+// focus first input initially
+inputs[0].focus();
 
-    num.addEventListener("keydown", (event) => {
-        // console.log(event);
-        // console.log(event.target);
-        // console.log(event.target.value);
-        // console.log(event.key);
-        if(event.key === "Backspace" && index > 0) {
-            // console.log("This is a number", num.value);
-            num.value = "";
-            otp[index-1].focus();
+inputs.forEach((input, index) => {
+
+    input.addEventListener("keydown", (e) => {
+
+        // Allow only single digit
+        if (e.key >= "0" && e.key <= "9") {
+            input.value = "";
+            setTimeout(() => {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                }
+            }, 10);
         }
-    })
-})
+
+        // Backspace behavior
+        if (e.key === "Backspace") {
+            if (input.value === "" && index > 0) {
+                inputs[index - 1].focus();
+            }
+        }
+    });
+});
