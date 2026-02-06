f4e61db51407bade7187cb06581a5b741c23f505
Narendra Sharma <154484787+NarendraSharma52@users.noreply.github.com>
2026-02-06 09:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 //your JS code here. If required.
-const otp = document.querySelectorAll(".code");
-// console.log(otp);
-// console.log(otp[0]);
-// console.dir(otp[0]);
-otp[0].focus();
+const inputs = document.querySelectorAll(".code");
 
-otp.forEach((num, index) => {
-    // console.log(num, index);
-    num.addEventListener("input", (event) => {
-        // console.dir(event.target);
-        console.log(event.target.value);
-        if(event.target.value.length === 1 && index < otp.length - 1) {
-            otp[index+1].focus();
+inputs.forEach((input, index) => {
+    input.addEventListener("input", () => {
+        // Move forward when a number is typed
+        if(input.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
         }
-    })
+    });
 
-    num.addEventListener("keydown", (event) => {
-        // console.log(event);
-        // console.log(event.target);
-        // console.log(event.target.value);
-        // console.log(event.key);
-        if(event.key === "Backspace" && index > 0) {
-            // console.log("This is a number", num.value);
-            num.value = "";
-            otp[index-1].focus();
+    input.addEventListener("keydown", (e) => {
+        if(e.key === "Backspace") {
+            if(input.value === "" && index > 0) {
+                // Move back if current input is empty
+                inputs[index - 1].focus();
+            }
         }
-    })
-})
+    });
+
+    // Optional: prevent non-numeric input
+    input.addEventListener("keypress", (e) => {
+        if(!/[0-9]/.test(e.key)) {
+            e.preventDefault();
+        }
+    });
+});
